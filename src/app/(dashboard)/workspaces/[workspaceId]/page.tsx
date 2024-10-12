a66b1c30dadc21898387b22aa3fadf1f4b20fa25
Chinmay-innovates <chinmaygope69@gmail.com>
2024-10-12 22:57:22 +0530

    add authentication individual route page

--- src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
@@ -1,4 +1,9 @@
-const WorkspaceIdpage = () => {
+import { getCurrent } from "@/features/auth/queries";
+import { redirect } from "next/navigation";
+
+const WorkspaceIdpage = async () => {
+	const current = await getCurrent();
+	if (!current) redirect("/sign-in");
 	return <div>WorkspaceIdpage</div>;
 };
 
--- src/app/(standalone)/workspaces/create/page.tsx
+++ src/app/(standalone)/workspaces/create/page.tsx
@@ -1,7 +1,10 @@
+import { redirect } from "next/navigation";
+import { getCurrent } from "@/features/auth/queries";
 import { CreateWorkspaceForm } from "@/features/workspaces/components/create-workspace-form";
-import React from "react";
 
-const CreateWorkspacePage = () => {
+const CreateWorkspacePage = async () => {
+	const current = await getCurrent();
+	if (!current) redirect("/sign-in");
 	return (
 		<div className="w-full lg:max-w-xl">
 			<CreateWorkspaceForm />
