ccf92f63e396b4d19ddb865737eec493cea600b5
Chinmay-innovates <chinmaygope69@gmail.com>
2024-11-02 12:47:35 +0530

    fix sign-up page

--- src/lib/oauth.ts
+++ src/lib/oauth.ts
@@ -1,20 +1,20 @@
 "use server";
 
-import {createAdminClient} from "@/lib/appwrite";
-import {redirect} from "next/navigation";
-import {headers} from "next/headers";
-import {OAuthProvider} from "node-appwrite";
+import { createAdminClient } from "@/lib/appwrite";
+import { redirect } from "next/navigation";
+import { headers } from "next/headers";
+import { OAuthProvider } from "node-appwrite";
 
 export async function signUpWithGithub() {
-    const {account} = await createAdminClient();
+	const { account } = await createAdminClient();
 
-    const origin = headers().get("origin");
+	const origin = headers().get("origin");
 
-    const redirectUrl = await account.createOAuth2Token(
-        OAuthProvider.Github,
-        `${origin}/oauth`,
-        `${origin}/sign-up`,
-    );
+	const redirectUrl = await account.createOAuth2Token(
+		OAuthProvider.Github,
+		`${origin}/oauth`,
+		`${origin}/sign-up`
+	);
 
-    return redirect(redirectUrl);
+	return redirect(redirectUrl);
 }
